95398afecc3c45bbd4440d95006945bc5176b3b8
José Martin Braconi Guzmán <martinbraconi@hotmail.com>
2024-07-31 14:43:40 -0500

    add: js/config-docsify.js

---
+++ docs/js/config-docsify.js
@@ -0,0 +1,34 @@
+window.$docsify = {
+    nameLink: '/',
+    search: 'auto',
+    search: {
+        noData: {
+            '/': 'No results!'
+        },
+        placeholder: {
+            '/': 'Search'
+        }
+    },
+    auto2top: true,
+    loadNavbar: true,
+    relativePath: true,
+    loadSidebar: true,
+    name: "Goldendog Linux",
+    darklightTheme: {
+        /*defaultTheme : 'dark',*/
+        dark: {
+            accent: '#9e6eef',
+            background: "#0d1117",
+            backgroundli: '#161b23',
+            scrollColorPrim: "#3a3a5d",
+            scrollColorSec: "#d6d6d6",
+        },
+        light: {
+            accent: '#9e6eef',
+            backgroundli: '#efefef',
+            scrollColorPrim: "#8c8b86",
+            scrollColorSec: "#d6d6d6",
+
+        }
+    }
+}
--- docs/js/searchColorShadowFix.js
+++ docs/js/searchColorShadowFix.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var inputs = document.querySelectorAll(".input-wrap");
 
     // Log the selected inputs to the console
-    console.log(inputs[0].children[0]);
+    // console.log(inputs[0].children[0]);
     
     inputs[0].children[0].addEventListener("focus", function() {
         this.style.boxShadow = "0 0 5px var(--theme-color, #9e6eef)"; // Establecer box-shadow en "none" al obtener el foco
